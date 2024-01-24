2fd15ab0b1e83070883d7a9d81398aa159366df6
bhujbal pratik <pratikbhujbal743@gmail.com>
2024-01-24 20:11:10 +0530

    Error Handling middleware

--- server/index.js
+++ server/index.js
@@ -11,6 +11,15 @@ const app = express()
 app.use(express.json())
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500
+  const message = err.message || "Internal Server Error"
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  })
+})
 
 app.get("/", (req, res) => {
   res.send("Home")
