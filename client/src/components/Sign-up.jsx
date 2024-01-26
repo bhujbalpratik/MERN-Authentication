c13e9d04d77b0533170683702ec30871d1ef54b2
bhujbal pratik <pratikbhujbal743@gmail.com>
2024-01-26 17:08:34 +0530

    SignUp Functionality Created

--- client/src/components/Sign-up.jsx
+++ client/src/components/Sign-up.jsx
@@ -1,33 +1,72 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, json } from "react-router-dom"
 
 const Signup = () => {
+  const [formData, setFormData] = useState({})
+
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value })
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    try {
+      setLoading(true)
+      setError(false)
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      })
+      const data = await res.json()
+      console.log(data)
+      setLoading(false)
+      if (data.success == false) {
+        setError(true)
+        return
+      }
+    } catch (error) {
+      setLoading(false)
+      setError(true)
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={submitHandler}>
         <input
           type="text"
           id="username"
           placeholder="Username"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="email"
           id="email"
           placeholder="Email"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="password"
           id="password"
           placeholder="Password"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <button
-          disabled={false}
+          disabled={loading}
+          type="submit"
           className="bg-slate-900 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-60 disabled:cursor-no-drop"
         >
-          Sign Up
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
 
@@ -37,6 +76,7 @@ const Signup = () => {
           <span className="text-blue-500">Sign in</span>
         </Link>
       </div>
+      <p className="text-red-600">{error && "User Already Exist!"}</p>
     </div>
   )
 }
