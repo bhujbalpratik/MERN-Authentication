6f08c44b3fbc63b52a940b676a1bbfe644f7b304
bhujbal pratik <pratikbhujbal743@gmail.com>
2024-02-05 14:17:36 +0530

    Sign in Functionality Created

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,5 +1,7 @@
 import bcryptjs from "bcryptjs"
 import { User } from "../models/user.models.js"
+import { errorHandler } from "../utils/errorHandler.js"
+import jwt from "jsonwebtoken"
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body
@@ -8,6 +10,26 @@ export const signup = async (req, res, next) => {
   try {
     await Newuser.save()
     res.status(201).json({ message: "User Created Successfully" })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body
+
+  try {
+    const validUser = await User.findOne({ email })
+    if (!validUser) return next(errorHandler(404, "User Not Found"))
+    const validPassword = bcryptjs.compareSync(password, validUser.password)
+    if (!validPassword) return next(errorHandler(401, "Invalid Password"))
+    const { password: hashedPassword, ...rest } = validUser._doc
+    const token = jwt.sign({ id: validUser._id }, process.env.SECURITY_KEY)
+
+    res
+      .cookie("token", token, { httpOnly: true, maxAge: 60 * 60 * 1000 })
+      .status(200)
+      .json(rest)
   } catch (error) {
     next(error)
   }
